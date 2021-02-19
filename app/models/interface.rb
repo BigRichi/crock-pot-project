faff55b8d54e9f9b2e77a91ca7fd3d33e31fbf52
Max Miller <maxmiller413@gmail.com>
2021-02-19 09:01:02 -0600

    updated comments

--- app/models/interface.rb
+++ app/models/interface.rb
@@ -1,13 +1,15 @@
 class Interface
 
+#attr macros for future methods
     attr_reader :prompt, :protein, :recipe, :recipe_ingredient
     attr_accessor :user, :recipe_ingredient_arr
 
-
+#Initalize to use TTY
     def initialize
         @prompt = TTY::Prompt.new
     end 
 
+#Welcome menu, including Logo, to clear seed data and screen -- takes you to login page after sleeping
     def welcome
         system 'rake db:seed'
         system 'clear'
@@ -18,6 +20,7 @@ class Interface
         login_page
     end 
 
+#Login page to login, sign up, or exit based on user's choice using TTY Select
     def login_page
         prompt.select("Login or Sign up") do |menu|
             menu.choice "Login", -> {login_helper}
@@ -26,6 +29,7 @@ class Interface
         end
     end
 
+#Enter previous username and password -- if correct, takes you to main menu, if incorrect, states incorrect and takes you back to login page (CRUD - Read)
     def login_helper
         username = prompt.ask("Enter Username")
         password = prompt.ask("Enter Password")
@@ -42,6 +46,7 @@ class Interface
         end
     end
 
+#Sign up with brand new username and password (CRUD ITEM - Create)
     def sign_up_helper
         username = prompt.ask("Enter Username")
         while User.find_by(name: username.downcase)
@@ -57,6 +62,7 @@ class Interface
         main_menu
     end
 
+#Once login successful, use TTY Select to choose protein, exit or delete
     def main_menu
         prompt.select("What would you like to do today #{user.name}?") do |menu|
             menu.choice "Choose Protein", -> {choose_protein}
@@ -65,12 +71,14 @@ class Interface
         end
     end
 
+#Exit helper
     def exit_helper
         puts "Goodbye 👋"
         sleep(2)
         system 'clear'
     end
 
+#Delete user's account (CRUD - Delete)
     def delete_account_helper
         current_user = User.find(self.user.id)
         current_user.delete
@@ -79,6 +87,7 @@ class Interface
         welcome
     end
 
+#Choose protein based on "each" enumerable and TTY select
     def choose_protein
         system 'clear'
         prompt.select("Which protein would you like to see recipes for?") do |menu|
@@ -88,17 +97,19 @@ class Interface
         end
     end
 
+#Choose protein recipe based on ActiveRecord "where" and TTY select (CRUD - Read)
     def main_menu_protein(protein_id)
         system 'clear'
         prompt.select("Main Menu") do |menu|
-            Recipe.all.where(protein_id: protein_id).each do |recp|
-                menu.choice recp.name.capitalize, -> {user_recipe_connection(recp.id)}
+            Recipe.all.where(protein_id: protein_id).each do |recipe|
+                menu.choice recipe.name.capitalize, -> {user_recipe_connection(recipe.id)}
             end
             menu.choice "Change Protein", -> {choose_protein}
             menu.choice "Log Off", -> {exit_helper}
         end
     end
 
+#Based on recipe id argument, if an instance is already created use it, if not create it (CRUD - Read & Create)
     def user_recipe_connection(recipe_id)
         if user.recipes.where(id: recipe_id) == Recipe.all.where(id: recipe_id)
             recipe_finder(recipe_id)
@@ -108,6 +119,7 @@ class Interface
         end
     end
 
+#Once a recipe is selected based on user and protein, puts the recipe attributes to the screen and allows TTY select options for see ingredient, choose different protein to go back or exit app
     def recipe_finder(recipe_id)
         system 'clear'
         Recipe.all.select{|recipe| recipe.id == recipe_id}.each do |i|
@@ -123,7 +135,8 @@ class Interface
             menu.choice "Exit", -> {exit_helper}
         end
     end
-    
+
+#Recipe helper to place recipe attributes to the screen 
     def recipe_status_helper(recipe_id)
         system 'clear'
         Recipe.all.select{|recipe| recipe.id == recipe_id}.each do |i|
@@ -134,12 +147,14 @@ class Interface
         end
     end
 
+#Gives user the option via TTY select to update recipe rating, add ingredient to recipe or remove ingredient from recipe -- as well as choose new protein to go back or exit app
     def recipe_ingredients(recipe_id)
         recipe_status_helper(recipe_id)
         @recipe_ingredient_arr = recipe_ingredient.split(", ")
         puts " "
         p user.recipes.where(id: recipe_id)[0].ingredient
         prompt.select("Options") do |menu|
+        puts " "
             menu.choice "Choose New Protein", -> {choose_protein}
             menu.choice "Update Recipe Rating", -> {update_recipe_rating(recipe_id)}
             menu.choice "Add Ingredient", -> {ingredient_add_helper(recipe_id)}
@@ -148,18 +163,21 @@ class Interface
         end
     end 
 
+#Based on the recipe and protein the user has chosen, this uses TTY Ask to give the user the ability to add a new rating to the recipe (CRUD - Read & Update)
     def update_recipe_rating(recipe_id)
         new_rating = prompt.ask("what new rating would you like to give?")
         self.user.recipes.where(id: recipe_id).update(rating: new_rating.to_i)
         recipe_ingredients(recipe_id)
     end
 
+#Based on the recipe and protein the user has chosen, this uses TTY Ask to give the user the ability to add a new ingredient to the recipe (CRUD - Read & Update)
     def ingredient_add_helper(recipe_id)
         added_ingredient = prompt.ask("What ingredient would you like to add")
         new_recipe_ingredient_arr = user.recipes.where(id: recipe_id)[0].ingredient.split(", ") << added_ingredient
         ingredient_change_helper(recipe_id, new_recipe_ingredient_arr)
     end 
 
+#Based on the recipe and protein the user has chosen, this uses TTY Multi-Select to give the user the ability to remove any existing ingredient(s) from the recipe (CRUD - Read)
     def ingredient_remove_helper(recipe_id)
         new_recipe_ingredient_arr = prompt.multi_select("Please choose from the list, ingredients you would like to keep") do |menu|
             menu.choices  user.recipes.where(id: recipe_id)[0].ingredient.split(", ")
@@ -167,7 +185,9 @@ class Interface
         ingredient_change_helper(recipe_id, new_recipe_ingredient_arr)
     end
 
+#Based on the recipe and protein the user has chosen, this updates the added inredient to the screen (CRUD - Read & Update)
     def ingredient_change_helper(recipe_id, new_recipe_ingredient_arr)
+        puts " "
         p "Ingredient(s) successfully updated!"
         sleep(1.3)
         user.recipes.where(id: recipe_id).update(ingredient: new_recipe_ingredient_arr.join(", "))
